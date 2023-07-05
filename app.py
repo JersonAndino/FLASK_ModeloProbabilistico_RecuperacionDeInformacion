2898f6f7d9ad988b90fd4b78aa0f9fc75ea1bdc3
Jerson Andino <albeiro.andino@gmail.com>
2023-07-05 17:14:25 -0500

    correccion de orden similitud

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ app = Flask(__name__)
 
 documentos=[]
 documentos_limpios=[]
-stop_words=['a','al','con','de','del','el','en','es','están','la','los','las','su','un','una','unos','unas','tiene','va','y']
+nombres_docs=[]
+stop_words=['a','al','con','de','del','el','en','es','están','la','los','las','su','un','una','unos','unas','tiene','va','y','que']
 V = []
 frecuencias=[]
 ni=[]
@@ -26,14 +27,34 @@ qi=[]
 ci=[]
 
 def limpiar_doc(documento):
+    """_summary_
+    author: Jerson Andino
+    Args:
+        documento (string): Recibe como parametro el texto del documento ingresadi por teclado o desde archivo
+
+    Returns:
+        doc: retorna una lista que contiene las palabras del documento sin stop_words y separada por espacios.
+    """
     documento=documento.lower()
     doc = documento.split()
+    for i,word in enumerate(doc):
+        if word.lower() in stop_words:
+            doc.pop(i)
     for i,word in enumerate(doc):
         if word.lower() in stop_words:
             doc.pop(i)
     return doc
 
 def vectorizar(documento,V):
+    """_summary_
+    author: Jerson Andino
+    Args:
+        documento (string): documento limpio perteneciente a la consulta
+        V (lista): el lexico correspondiente a los documentos cargados al sistema
+
+    Returns:
+        q_v: retorna una lista correspondiente al vector de la consulta
+    """
     q_v=[]
     for word in V:
         if word in documento:
@@ -42,8 +63,14 @@ def vectorizar(documento,V):
             q_v.append(0)
     return q_v
 ##############################################
-# Calcular
+# Funciones de calculo de tablas
 def calcular_v(documentos_limpios):
+    """Calcula el lexico actual respectoa los documentos cargados de forma iterativa
+    
+    author: Jerson Andino
+    Args:
+        documentos_limpios (lista): una lista de documentos después de haber pasado por el proceso de limpieza
+    """
     V.clear()
     if documentos_limpios:
         for d in documentos_limpios:
@@ -52,10 +79,15 @@ def calcular_v(documentos_limpios):
                     V.append(word)  
     else:
         V.clear()
-    # print(V)
 
 def calcular_frecuencias(documentos_limpios, V):
-    # print(D)
+    """Calculo de las frecuencias de cada palabra del documento encontradas en el documento respecto al lexico,
+    actualiza la lista de frecuencias
+
+    Args:
+        documentos_limpios (lista): una lista de documentos después de haber pasado por el proceso de limpieza
+        V (_type_): el lexico correspondiente a los documentos cargados al sistema
+    """
     frecuencias.clear()
     for d in documentos_limpios:
         d_temp=d
@@ -67,10 +99,14 @@ def calcular_frecuencias(documentos_limpios, V):
                     cont+=1
             d_v_temp.append(cont)
         frecuencias.append(d_v_temp)
-        # print(D2)
-    # print(frecuencias)
 
 def calcular_ni(frecuencias,V):
+    """Calculo de el valor ni para la tabla de frecuencias
+
+    Args:
+        frecuencias (lista): Lista de frecuencias de palabras en documentos
+        V (_type_): Lexico de los documentos cargados
+    """
     ni.clear()
     for i in range(len(V)):
         cont=0
@@ -80,19 +116,36 @@ def calcular_ni(frecuencias,V):
         ni.append(cont)
 
 def calcular_qi(ni):
+    """Calculo del elemento qi en la tabla de similitud
+
+    Args:
+        ni (lista): Lista que contiene la frecuencia de cada palabra en los documentos
+    """
     qi.clear()
     N=len(documentos)
-    # print(N)
     for n in ni:
         qi.append(n/N)
         
 def calcular_ci(ni):
+    """Calculo del valor ci, el peso de relevancia de cada palabra de los documentos
+
+    Args:
+        ni (lista): Lista que contiene la frecuencia de cada palabra en los documentos
+    """
     ci.clear()
     N=len(documentos)    
     for n in ni:
         ci.append(log((N-n)/n,10))
     
-def calcular_tabla(documentos):
+def calcular_tabla():
+    """Union de todas las tablas pertinentes calculadas para armar la tabla de similitud
+
+    Args:
+        
+
+    Returns:
+        tabla_pesos: una lista compuesta de distintos valores, cada fila contiene los valores pertinentes a todas las tablas
+    """
     tabla_pesos=[]
     try:
         if len(documentos)>1:
@@ -117,6 +170,14 @@ def calcular_tabla(documentos):
     return tabla_pesos
 ###################################################3
 def ordenar_sim(sim):
+    """Realizar el ordenamiento de los documentos segun su similitud
+
+    Args:
+        sim (lista): Una lista que contiene los valores resultantes de similitud de cada documento
+
+    Returns:
+        nuevo_orden: Una lista que contiene el orden de los documentos segun su relevancia
+    """
     orden=list(range(len(sim)))
     nuevo_orden=[]
     for i in range(len(sim)):
@@ -129,27 +190,42 @@ def ordenar_sim(sim):
         sim.pop(maxi)
         nuevo_orden.append(orden[maxi])
         orden.pop(maxi)
+        
+    tabla_orden=[]
+    for p in nuevo_orden:
+        tabla_orden.append(['d'+str(p+1),nombres_docs[p]])
     
-    return nuevo_orden
+    return tabla_orden
 
 def consultar_q(frecuencias, ci, q):
-    D3=[]
+    """Realiza el calculo de la tabla de similitud
+
+    Args:
+        frecuencias (lista): lista de frecuencias de las palabras de cada documento
+        ci (lista): lista de pesos de relevancia de cada palabra en el lexico
+        q (lista): vector de la consulta realizada
+
+    Returns:
+        sim: lista de similitud obtenida
+        orden: nuevo orden calculado para la relevancia de documentos
+    """
+    producto_interno=[]
     n=len(V)
     for d in frecuencias:
         d_temp=[]
         for i in range(n):
             d_temp.append(q[i]*d[i])
-        D3.append(d_temp)
-    D4=[]    
-    for d in D3:
+        producto_interno.append(d_temp)
+    producto_interno_pesos=[]    
+    for d in producto_interno:
         d_temp=[]
         print(d)
         print(ci)
         for i in range(n):
             d_temp.append(d[i]*ci[i])
-        D4.append(d_temp)  
+        producto_interno_pesos.append(d_temp)  
     sim=[]  
-    for d in D4:
+    for d in producto_interno_pesos:
         sim.append(abs(sum(d)))
     sim_copy=sim.copy()    
     return (sim, ordenar_sim(sim_copy))      
@@ -167,18 +243,15 @@ def home():
                 inicio = time.time_ns()
                 (sim, orden_docs) = consultar_q(frecuencias,ci,q)
                 fin = time.time_ns()
-                print(inicio)
-                print(fin)
-                print(fin-inicio)
                 content['sim']=sim
                 content['orden_docs']=orden_docs    
                 content['consulta'] = consulta   
-                content['tiempo'] = fin-inicio        
+                content['tiempo'] = (fin-inicio)/10**(9)        
             
     calcular_v(documentos_limpios)
     calcular_frecuencias(documentos_limpios,V)
     calcular_ni(frecuencias,V)
-    tabla_pesos = calcular_tabla(documentos)
+    tabla_pesos = calcular_tabla()
     content['ed']=len(documentos)
     content['docs']=documentos
     content['V']=V
@@ -199,23 +272,27 @@ def cargar_documento():
                 filename = secure_filename(f.filename)
                 file_path=os.path.join('uploaded_files', filename)
                 f.save(file_path)
-                file=open(file_path)
+                file=open(file_path,encoding="utf-8")
                 doc = file.read()
                 file.close()
                 documentos.append(doc)
                 documentos_limpios.append(limpiar_doc(doc))
+                nombres_docs.append(filename)
             else:
                 if request.form['texto']:
                     documentos.append(request.form['texto'])
                     documentos_limpios.append(limpiar_doc(request.form['texto']))
+                    nombres_docs.append(request.form['texto'])
         if request.form['tipo_post']=='1':
             index_doc = int(request.form['doc_index'])
             documentos.pop(index_doc-1)
             documentos_limpios.pop(index_doc-1)
+            nombres_docs.pop(index_doc-1)
             
         if request.form['tipo_post']=='2':
             documentos.clear()
             documentos_limpios.clear()
+            nombres_docs.clear()
             ci.clear()
             V.clear()
             ni.clear()
@@ -230,7 +307,7 @@ def cargar_documento():
 @app.route('/calcular', methods=['GET'])
 def calcular():
     content = dict()
-    tabla_pesos = calcular_tabla(documentos)
+    tabla_pesos = calcular_tabla()
     content['ed']=len(documentos)
     content['docs']=documentos
     content['V']=V
